b6a4b9dc25e88bd4bacaf1844b70032cf6aab803
noangel <angeljanai@gmail.com>
2012-05-22 11:50:13 +0900

    patch iOS-build.sh to work with latest Xcode 4.3.2

--- iOS-build.sh
+++ iOS-build.sh
@@ -5,11 +5,11 @@ set -o errexit
 # http://randomsplat.com/id5-cross-compiling-python-for-embedded-linux.html
 # http://latenitesoft.blogspot.com/2008/10/iphone-programming-tips-building-unix.html
 
-export IOS_VERSION="4.3"
+export IOS_VERSION="5.1"
 
 # download python and patch if they aren't there
 if [[ ! -a Python-2.6.5.tar.bz2 ]]; then
-    curl http://www.python.org/ftp/python/2.6.5/Python-2.6.5.tar.bz2 > Python-2.6.5.tar.bz2
+curl http://www.python.org/ftp/python/2.6.5/Python-2.6.5.tar.bz2 > Python-2.6.5.tar.bz2
 fi
 
 # get rid of old build
@@ -30,26 +30,26 @@ make distclean
 patch -p1 < ../Python-2.6.5-xcompile.patch
 
 # set up environment variables for simulator compilation
-export DEVROOT="/Developer/Platforms/iPhoneSimulator.platform/Developer"
+export DEVROOT="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer"
 export SDKROOT="$DEVROOT/SDKs/iPhoneSimulator${IOS_VERSION}.sdk"
 
 if [ ! -d "$DEVROOT" ]; then
-    echo "DEVROOT doesn't exist. DEVROOT=$DEVROOT"
+echo "DEVROOT doesn't exist. DEVROOT=$DEVROOT"
     exit 1
 fi
 
 if [ ! -d "$SDKROOT" ]; then
-    echo "SDKROOT doesn't exist. SDKROOT=$SDKROOT"
+echo "SDKROOT doesn't exist. SDKROOT=$SDKROOT"
     exit 1
 fi
 
-export CPPFLAGS="-I$SDKROOT/usr/lib/gcc/arm-apple-darwin10/4.2.1/include/ -I$SDKROOT/usr/include/"
+export CPPFLAGS="-I$SDKROOT/usr/lib/gcc/arm-apple-darwin11/4.2.1/include/ -I$SDKROOT/usr/include/"
 export CFLAGS="$CPPFLAGS -pipe -no-cpp-precomp -isysroot $SDKROOT"
 export LDFLAGS="-isysroot $SDKROOT"
 export CPP="/usr/bin/cpp $CPPFLAGS"
 
 # build for iPhone Simulator
-./configure CC="$DEVROOT/usr/bin/i686-apple-darwin10-llvm-gcc-4.2 -m32" \
+./configure CC="$DEVROOT/usr/bin/i686-apple-darwin11-llvm-gcc-4.2 -m32" \
             LD="$DEVROOT/usr/bin/ld" --disable-toolbox-glue --host=i386-apple-darwin --prefix=/python
 make HOSTPYTHON=./hostpython HOSTPGEN=./Parser/hostpgen \
      CROSS_COMPILE_TARGET=yes
@@ -59,16 +59,16 @@ mv libpython2.6.a libpython2.6-i386.a
 make distclean
 
 # set up environment variables for cross compilation
-export DEVROOT="/Developer/Platforms/iPhoneOS.platform/Developer"
+export DEVROOT="/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer"
 export SDKROOT="$DEVROOT/SDKs/iPhoneOS${IOS_VERSION}.sdk"
 
 if [ ! -d "$DEVROOT" ]; then
-    echo "DEVROOT doesn't exist. DEVROOT=$DEVROOT"
+echo "DEVROOT doesn't exist. DEVROOT=$DEVROOT"
     exit 1
 fi
 
 if [ ! -d "$SDKROOT" ]; then
-    echo "SDKROOT doesn't exist. SDKROOT=$SDKROOT"
+echo "SDKROOT doesn't exist. SDKROOT=$SDKROOT"
     exit 1
 fi
 
